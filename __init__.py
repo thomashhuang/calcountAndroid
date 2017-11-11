b444da32f90a84525d5380d6a58b415496a21eeb
Thomas Huang <thomas.huang2017@gmail.com>
2017-11-11 13:53:06 -0600

    Flask server now supports POST requests.

--- __init__.py
+++ __init__.py
@@ -2,20 +2,17 @@ from flask import Flask, request, render_template
 
 app = Flask(__name__)
 
-
 @app.route('/')
 def index():
-    return 'Hello!'
+    return 'POST to /recognize with a link to the image or visit /test to test.'
 
 @app.route('/recognize', methods=['GET', 'POST'])
 def recognize():
     if request.method == 'GET':
         return 'Upload a file to this URL to process.'
     elif request.method == 'POST':
-        image = request.data
-        #Not yet supported
-        #return render_template('ImageRecognition.html', input_image = image
-        pass
+        link_to_image = request.data
+        return render_template('ImageRecognition.html', link = link_to_image)
 
 @app.route('/test')
 def test():
