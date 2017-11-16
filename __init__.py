b0771ebe9a77c245910f21be10c92440926d4662
Thomas Huang <thomas.huang2017@gmail.com>
2017-11-16 14:48:23 -0600

    Created separate script for recognition logic.

--- __init__.py
+++ __init__.py
@@ -9,14 +9,14 @@ def index():
 @app.route('/recognize', methods=['GET', 'POST'])
 def recognize():
     if request.method == 'GET':
-        return 'Make a POST request to this URL to process.'
+        return render_template('ImageRecognition.html', link = 'http://2.bp.blogspot.com/_LOn5giboI0Q/TCrA_r7b7gI/AAAAAAAAELA/vzPzmtKgY14/s1600/IMG_1117.JPG')
     elif request.method == 'POST':
         link_to_image = request.data
-        return render_template('ImageRecognition.html', link = 'http://2.bp.blogspot.com/_LOn5giboI0Q/TCrA_r7b7gI/AAAAAAAAELA/vzPzmtKgY14/s1600/IMG_1117.JPG')
+        return 'Make a POST request to this URL to process.'
 
 @app.route('/test')
 def test():
-    return render_template('ImageRecognitionTest.html', link = 1)
+    return render_template('ImageRecognitionTest.html', index = 0)
 
 if __name__ == '__main__':
     app.run()
